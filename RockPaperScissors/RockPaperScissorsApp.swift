a1e2100437f080c1b98793f2edaf18be1bf98812
prosvetovvv <vp12@yandex.ru>
2021-02-24 14:59:32 +0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Vitaly Prosvetov on 24.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
